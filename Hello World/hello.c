29046dc64ab848161e823a4ab43301a3921ba46c
Akif Manzoor <akifmanzoor124@gmail.com>
2019-12-07 13:57:58 -0500

    added comments to the code, explaining function of statements

--- Hello World/hello.c
+++ Hello World/hello.c
@@ -1,7 +1,23 @@
+/*
+    stdio.h is a header file for the file stdio.c
+    To use the file, we use statement #include
+*/
 #include <stdio.h>
+
+/*
+    every C code needs to start with the main function
+    Mandatory in every program
+*/
 
 int main()
 {
+    /*
+        stdio.c contains the function printf which prints to the console
+    */
     printf("Hello World!!\n");
+
+    /*
+        Not mandatory but is good programming practice
+    */
     return 0;
 }
